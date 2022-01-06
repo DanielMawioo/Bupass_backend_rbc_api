137035ad335a9fe39c5aaeaa93f4d4849e548af1
DanielMawioo <mawioodaniel43@gmail.com>
2022-01-06 11:46:21 +0300

    update settings and models

--- api/models.py
+++ api/models.py
@@ -20,30 +20,28 @@ class User(AbstractBaseUser, PermissionsMixin):
         (MANAGER, 'Manager'),
         (EMPLOYEE, 'Employee')
     )
-    
+
     class Meta:
         verbose_name = 'user'
         verbose_name_plural = 'users'
 
-  # Roles created here
-  uid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
-  email = models.EmailField(unique=True)
-  first_name = models.CharField(max_length=30, blank=True)
-  last_name = models.CharField(max_length=50, blank=True)
-  role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True, default=3)
-  date_joined = models.DateTimeField(auto_now_add=True)
-  is_active = models.BooleanField(default=True)
-  is_deleted = models.BooleanField(default=False)
-  created_date = models.DateTimeField(default=timezone.now)
-  modified_date = models.DateTimeField(default=timezone.now)
-  created_by = models.EmailField()
-  modified_by = models.EmailField()
+    uid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True, default=7)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+    created_by = models.EmailField()
+    modified_by = models.EmailField()
 
-  USERNAME_FIELD = 'email'
-  REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
     def __str__(self):
         return self.email
-  
